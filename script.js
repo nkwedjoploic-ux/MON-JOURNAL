4400543942a55f63331118d6cde23af32da0fab0
nkwedjoploic-ux <nkwedjoploic@gmail.com>
2026-08-17 00:38:55 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,35 +1,7 @@
-function inscription() {
-  const nom = document.getElementById("nom").value.trim();
-  const prenom = document.getElementById("prenom").value.trim();
-  const password = document.getElementById("password").value;
-
-  if (!nom ||!prenom ||!password) {
-    alert("Veuillez remplir tous les champs.");
-    return;
-}
-
-  const users = JSON.parse(localStorage.getItem("users")) || {};
-
-  if (users[password]) {
-    alert("Ce mot de passe est déjà utilisé. Choisissez-en un autre.");
-    return;
-}
-
-  users[password] = { nom, prenom};
-  localStorage.setItem("users", JSON.stringify(users));
-  localStorage.setItem("utilisateurActif", password);
-  window.location.href = "index2.html";
-}
-
-function connexion() {
-  const password = document.getElementById("loginPassword").value;
-  const users = JSON.parse(localStorage.getItem("users")) || {};
-
-  if (!users[password]) {
-    alert("Mot de passe incorrect ou compte inexistant.");
-    return;
-}
-
-  localStorage.setItem("utilisateurActif", password);
-  window.location.href = "index2.html";
-}
+const toastList = [].slice.call(document.querySelectorAll('.toast'));
+const finalList = toastList.map(function(element){
+    return new bootstrap.Toast(element);
+});
+finalList.forEach(element => element.show());
+const popoverTrigger = document.querySelectorAll('[data-bs-toggle="popover"]');
+const popoverList =[...popoverTrigger].map(element => new bootstrap.Popover(element));
